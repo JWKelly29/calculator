8a623d13876a889b1160107fed0a1241f60d8750
DarkChocLatte <darkchoclatte@gmail.com>
2017-02-19 12:00:05 +0000

    file modified: test_calculator.py. Created setup file

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -3,10 +3,14 @@ from app.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_calculator_add_method(self):
-        calc = Calculator()
         result = calc.add(2,2)
         self.assertEqual(4, result)
+
+
 
 if __name__ == '__main__':
     unittest.main()
